c364f364d8cdf5fb87131bff42611356350e1ad2
ryu-coding <ryu.coding1@gmail.com>
2024-02-12 07:38:39 +0900

    faet : codeit javascript

---
+++ JavaScript/codeit/2. Mordern JavaScript/how_to/training/and_or.js
@@ -0,0 +1,15 @@
+// 1.quiz
+// console.log("String" && 123);
+// console.log({} || []);
+// console.log(0 && false);
+// console.log(null || undefined);
+// console.log(NaN && "Codeit");
+// console.log("" || true);
+// console.log("JavaScript" && null);
+// console.log(3000 || undefined);
+
+// 2.quiz
+console.log("codeit" && undefined && null);
+console.log("codeit" || false || null);
+console.log(("codeit" && 123) || null);
+console.log("codeit" || (123 && true));
---
+++ JavaScript/codeit/2. Mordern JavaScript/how_to/training/variables_scope.js
@@ -0,0 +1,21 @@
+// 1. quiz
+// console.log(age);
+
+// var age = 3;
+
+// console.log(age);
+
+// 2. quiz
+const sum = 0;
+
+function getSum(x, y) {
+  const sum = x + y;
+
+  return sum;
+}
+
+if (getSum(1, 5) > 3) {
+  const sum = 4 + 6;
+}
+
+console.log(sum);

520676788291ba62b25a5784f5f9cba22ec15244
ryu-coding <ryu.coding1@gmail.com>
2024-03-05 13:38:53 +0900

    study : Node.js _20240304 - codeit

---
+++ Node_js/codeit/1.Node.js_basics/node_basic_concepts/core_module.js
@@ -0,0 +1,10 @@
+// * core module - example
+// const fs = require("fs");
+
+// let fileList = fs.readdirSync(".");
+// console.log(fileList);
+
+// fs.writeFileSync("new.js", "Hello Node.js!");
+const os = require("os");
+
+console.log(os.cpus());
---
+++ Node_js/codeit/1.Node.js_basics/node_basic_concepts/main.js
@@ -0,0 +1,10 @@
+// * default(create) module
+let m = require("./math_tools.js");
+
+// console.log(m.plus(1, 2));
+
+console.log(m.PI);
+console.log(m.add(2, 4));
+console.log(m.subtract(1, 2));
+console.log(m.multiply(1, 2));
+console.log(m.divide(1, 2));

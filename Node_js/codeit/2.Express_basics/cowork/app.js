a5ef903329ff34a7d526315ca345af7b7d984395
ryu-coding <ryu.coding1@gmail.com>
2024-03-09 21:05:24 +0900

    study : Node.js 20240308 - ORM

---
+++ Node_js/codeit/2.Express_basics/cowork/app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+
+const app = express();
+
+// let members = require("./members");
+
+const db = require("./models");
+
+const { Member } = db;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.query);
+  next();
+});
+
+app.get("/api/members", (req, res) => {
+  //   res.send("<h1>Hello Express</h1>");
+  //   res.send(members);
+  const { team } = req.query;
+  if (team) {
+    const teamMembers = members.filter((m) => m.team === team);
+    res.send(teamMembers);
+  } else {
+    res.send(members);
+  }
+});
+
+app.get("/api/members/:id", (req, res) => {
+  const { id } = req.params;
+  const member = members.find((m) => m.id === Number(id));
+  if (member) {
+    res.send(member);
+  } else {
+    res.status(404).send({ message: "There is no with the id" });
+  }
+});
+
+app.post("/api/members", (req, res) => {
+  //   console.log(req.body);
+  const newMember = req.body;
+  members.push(newMember);
+  res
+    .status(201)
+    .send({ message: "Member added successfully", member: newMember });
+});
+
+app.put("/api/members/:id", (req, res) => {
+  const { id } = req.params;
+  const newInfo = req.body;
+  const member = members.find((m) => m.id === Number(id));
+  if (member) {
+    Object.keys(newInfo).forEach((prop) => {
+      member[prop] = newInfo[prop];
+    });
+    res.send(member);
+  } else {
+    res.status(404).send({ message: "There is no member with the id!" });
+  }
+});
+
+app.delete("/api/members/:id", (req, res) => {
+  const { id } = req.params;
+  const membersCount = members.length;
+  members = members.filter((member) => member.id !== Number(id));
+  if (members.length < membersCount) {
+    res.send({ message: "Deleted" });
+  } else {
+    res.status(404).send({ message: "There is no member with the id!" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is listening...");
+});

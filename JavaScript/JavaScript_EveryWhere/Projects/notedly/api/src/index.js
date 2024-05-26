1b5bcf634ae960092654423bd73b88167747bf89
ryu-coding <ryu.coding1@gmail.com>
2024-05-26 16:45:14 +0900

    study: javascript_everywhere_20240526 ch.05,06,07,08,09,10

--- JavaScript/JavaScript_EveryWhere/Projects/notedly/api/src/index.js
+++ JavaScript/JavaScript_EveryWhere/Projects/notedly/api/src/index.js
@@ -1,9 +1,21 @@
 // index.js
 // This is the main entry point of our application
-const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+const jwt = require('jsonwebtoken');
+const helmet = require('helmet');
+const cors = require('cors');
+const depthLimit = require('graphql-depth-limit');
+const { createComplexityLimitRule } = require('graphql-validation-complexity');
+require('dotenv').config();
+
+const db = require('./db');
+const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
 
 let notes = [
   { id: '1', content: 'This is a note', author: 'Adam Scott' },
@@ -11,52 +23,33 @@ let notes = [
   { id: '3', content: 'Oh hey loo, another note!', author: 'Riley Harrison' }
 ];
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-    notes: [Note!]!
-    note(id: ID!): Note!
-  }
-  type Pizza {
-    id: ID!
-    size: String!
-    slices: Int!
-    toppings: [String]
-  }
-  type Note {
-    id: ID!
-    content: String!
-    author: String!
-  }
-  type Mutation {
-    newNote(content: String!): Note!
-  }
-`;
+const app = express();
+app.use(helmet());
+app.use(cors());
 
-const resolvers = {
-  Query: {
-    hello: () => 'Hello World',
-    notes: () => notes,
-    note: (parent, args) => {
-      return notes.find(note => note.id === args.id);
-    }
-  },
-  Mutation: {
-    newNote: (parent, args) => {
-      const newNote = {
-        id: String(notes.length + 1),
-        content: args.content,
-        author: 'Adam Scott'
-      };
-      notes.push(newNote);
-      return newNote;
+db.connect(DB_HOST);
+
+const getUser = token => {
+  if (token) {
+    try {
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      throw new Error('Session invaild');
     }
   }
 };
 
-const app = express();
-
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
+  context: ({ req }) => {
+    const token = req.headers.authorization;
+    const user = getUser(token);
+    console.log(user);
+    return { models, user };
+  }
+});
 
 server.applyMiddleware({ app, path: '/api' });
 

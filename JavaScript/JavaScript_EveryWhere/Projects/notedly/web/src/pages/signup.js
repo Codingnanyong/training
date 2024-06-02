f87c1a646432eef753d0c33beecc1936e7147df5
ryu-coding <ryu.coding1@gmail.com>
2024-06-02 11:26:06 +0900

    study : Javascript_everywhere chapter 13.14,15,16,17

---
+++ JavaScript/JavaScript_EveryWhere/Projects/notedly/web/src/pages/signup.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import { useMutation, useApolloClient } from "@apollo/client";
+
+import UserForm from "../components/UserForm";
+import { SIGNUP_USER } from "../gql/mutation";
+
+const SignUp = (props) => {
+    useEffect(() => {
+        document.title = "Sign Up — Notedly";
+    });
+
+    const client = useApolloClient();
+    const [signUp, { loading, error }] = useMutation(SIGNUP_USER, {
+        onCompleted: (data) => {
+            localStorage.setItem("token", data.signUp);
+            client.writeData({ data: { isLoggedIn: true } });
+            props.history.push("/");
+        },
+    });
+
+    return (
+        <React.Fragment>
+            <UserForm action={signUp} formType="signup" />
+            {loading && <p>Loading...</p>}
+            {error && <p>Error creating an account!</p>}
+        </React.Fragment>
+    );
+};
+
+export default SignUp;
